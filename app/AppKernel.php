5e896afbeb669604bd5d5f4b08e5816986cdd6f1
=Christian Wielath <info@script-brain.de>
2013-07-18 14:58:39 +0200

    init

---
+++ app/AppKernel.php
@@ -0,0 +1,53 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            
+            // SymBB, befor other Bundles
+            new SymBB\Core\ConfigBundle\SymBBCoreConfigBundle(),
+            new SymBB\Core\UserBundle\SymBBCoreUserBundle(),
+            new SymBB\Core\TemplateBundle\SymBBCoreTemplateBundle,
+            new SymBB\Core\AdminBundle\SymBBCoreAdminBundle(),
+            new SymBB\Core\ForumBundle\SymBBCoreForumBundle(),
+            
+            // SymBB Templates
+            new SymBB\Template\AcpBundle\SymBBTemplateAcpBundle(),
+            new SymBB\Template\SimpleBundle\SymBBTemplateSimpleBundle,
+            
+            // FOS
+            new FOS\UserBundle\FOSUserBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
