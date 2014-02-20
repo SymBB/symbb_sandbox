51a44494a9e1222210f34add022706ea4ffde9f4
Christian Wielath <christian.wielath@tyre24.de>
2014-02-20 17:29:37 +0100

    first template "extending" example

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,9 +22,9 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new JMS\AopBundle\JMSAopBundle()
         );
-        
+
         \SymBB\Core\InstallBundle\BundleLoader::loadBundles($bundles, $this);
-        
+
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -32,12 +32,35 @@ class AppKernel extends Kernel
         }
 
         return $bundles;
-
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
 
+    /**
+     * Initializes the data structures related to the bundle management.
+     *
+     *  - the bundles property maps a bundle name to the bundle instance,
+     *  - the bundleMap property maps a bundle name to the bundle inheritance hierarchy (most derived bundle first).
+     *
+     * @throws \LogicException if two bundles share a common name
+     * @throws \LogicException if a bundle tries to extend a non-registered bundle
+     * @throws \LogicException if a bundle tries to extend itself
+     * @throws \LogicException if two bundles extend the same ancestor
+     */
+    protected function initializeBundles()
+    {
+        parent::initializeBundles();
+
+        foreach ($this->registerBundles() as $bundle) {
+
+            if (\method_exists($bundle, "extendBundle")) {
+                $name = $bundle->getName();
+                $extenBundle = $bundle->extendBundle();
+                $this->bundleMap[$name][] = $this->bundles[$extenBundle];
+            }
+        }
     }
 }
