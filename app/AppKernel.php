90a7c64ddbb0dd809445d7dd95ec6f8129903fdb
Christian Wielath <christian.wielath@tyre24.de>
2014-05-14 16:34:21 +0200

    remove default bundles and move it to symbb bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,20 +15,7 @@ class AppKernel extends SymBB\Core\InstallBundle\Kernel
     {
         $parentBundles = parent::registerBundles();
         
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-        );
+        $bundles = array();
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
